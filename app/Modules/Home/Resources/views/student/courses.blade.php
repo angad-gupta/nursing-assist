f7cc413f19e2128e806f0bf8211c2f0ec1f8d2f4
Deepak Shrestha <deepak.shrestha@bidhee.com>
2020-06-30 11:56:12 +0545

    mockup design

--- app/Modules/Home/Resources/views/student/courses.blade.php
+++ app/Modules/Home/Resources/views/student/courses.blade.php
@@ -21,50 +21,91 @@
 <section class="neta-about student-hub section-padding">
     <div class="container">
         <div class="row">
-            <div class="col-sm-12">
-                <div class="my-courses">
-                    <h2 class="ttl-line">My Courses
-                    </h2>
-                    <p>Find your Courses and explode your knowledge through online and evaluate with taking online Practise Tests.</p>
-                    <div class="row">
+         <div class="col-sm-12">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">My Courses</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">My Mockups</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" id="contact-tab" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">Other Courses</a>
+                </li>
+              </ul>
+              <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="my-courses">
                         
-                         @if($student_course) 
-                            @foreach($student_course as $key => $my_course_val)
-
-                            @php
-                                $k = $key+1;
-                                $imgfluid = asset('home/img/c' .$k. '.png'); 
-
-                                $total_syllabus = App\Modules\CourseContent\Entities\CourseContent::gettotalsyllabus($my_course_val->courseinfo_id);
-                            @endphp
-
+                        <div class="row">
+                            
+                             @if($student_course) 
+                                @foreach($student_course as $key => $my_course_val)
+    
+                                @php
+                                    $k = $key+1;
+                                    $imgfluid = asset('home/img/c' .$k. '.png'); 
+    
+                                    $total_syllabus = App\Modules\CourseContent\Entities\CourseContent::gettotalsyllabus($my_course_val->courseinfo_id);
+                                @endphp
+    
+                                <div class="col-sm-6 col-md-4 col-lg-3">
+                                    <div class="my-courses__list">
+                                        <img src="{{ $imgfluid }}" class="img-fluid" alt="">
+                                        <div class="list-content">
+                                        <h5>{{ optional($my_course_val->courseInfo)->course_program_title }}</h5>
+                                        <span>{{$total_syllabus}} syllabus</span>
+                                        <div class="neta-limit"> <p>{!! optional($my_course_val->courseInfo)->short_content !!} </p> </div>
+                                         <button class="btn e-btn w-100"><a href="{{ route('syllabus-detail',['course_info_id'=>$my_course_val->courseinfo_id]) }}">View syllabus</a></button>
+                                        </div>
+                                    </div>
+                                </div>
+    
+                                @endforeach
+                                @endif
+    
+    
+                        </div>
+    
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <div class="my-courses">
+                        <div class="row">
                             <div class="col-sm-6 col-md-4 col-lg-3">
                                 <div class="my-courses__list">
-                                    <img src="{{ $imgfluid }}" class="img-fluid" alt="">
                                     <div class="list-content">
-                                    <h5>{{ optional($my_course_val->courseInfo)->course_program_title }}</h5>
-                                    <span>{{$total_syllabus}} syllabus</span>
-                                    <div class="neta-limit"> <p>{!! optional($my_course_val->courseInfo)->short_content !!} </p> </div>
-                                     <button class="btn e-btn w-100"><a href="{{ route('syllabus-detail',['course_info_id'=>$my_course_val->courseinfo_id]) }}">View syllabus</a></button>
+                                    <h5>Mockup Test 1</h5>
+                                    <span>15 Questions</span>
+                                    <button class="btn e-btn w-100"><a href="#">View detail</a></button>
                                     </div>
                                 </div>
                             </div>
 
-                            @endforeach
-                            @endif
-
-
-                    </div>
+                            <div class="col-sm-6 col-md-4 col-lg-3">
+                                <div class="my-courses__list">
+                                    <div class="list-content">
+                                    <h5>Mockup Test 1</h5>
+                                    <span>105 Questions</span>
+                                    <button class="btn e-btn w-100"><a href="#">View detail</a></button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-md-4 col-lg-3">
+                                <div class="my-courses__list">
+                                    <div class="list-content">
+                                    <h5>Mockup Test 1</h5>
+                                    <span>15 Questions</span>
+                                    <button class="btn e-btn w-100"><a href="#">View detail</a></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
                 </div>
-            </div>
-
-
-            <div class="col-sm-12">
-                <div class="my-courses">
-                    <h2 class="ttl-line">Other Courses
-                    </h2>
-                    <p>Browse Online Courses including a mockup of each course.</p>
+                </div>
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                    <div class="my-courses">
                         <div class="row">
 
                         @if($other_course)
@@ -96,8 +137,9 @@
                         </div>
 
                 </div>
+                </div>
             </div>
-
+        </div>
         </div>
     </div>
 </section>
