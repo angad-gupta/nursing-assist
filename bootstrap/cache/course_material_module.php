064bb27581f80d3d3eb78bbcd79c8cbd44f2735b
Deepak Shrestha <deepak.shrestha@bidhee.com>
2020-07-05 11:27:16 +0545

    css changes 7

---
+++ bootstrap/cache/course_material_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'App\\Modules\\CourseMaterial\\Providers\\CourseMaterialServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'App\\Modules\\CourseMaterial\\Providers\\CourseMaterialServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
