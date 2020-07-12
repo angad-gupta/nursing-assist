75e2a4c4c256695a06a3b634277cd38a0998b222
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-07-12 16:25:57 +0545

    test

--- app/Modules/Home/Resources/views/student/courses.blade.php
+++ app/Modules/Home/Resources/views/student/courses.blade.php
@@ -27,7 +27,7 @@
                 <li class="nav-item">
                   <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">My Courses</a>
                 </li>
-                @if($student_course) 
+                @if(sizeof($student_course)>0) 
                 <li class="nav-item">
                   <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Mock Tests</a>
                 </li>
