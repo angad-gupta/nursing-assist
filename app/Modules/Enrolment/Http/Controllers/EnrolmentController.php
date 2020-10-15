a033a8c5ebece15b74ea71b26ac5861bdff2b307
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-10-15 13:11:09 +0545

    Test

--- app/Modules/Enrolment/Http/Controllers/EnrolmentController.php
+++ app/Modules/Enrolment/Http/Controllers/EnrolmentController.php
@@ -98,7 +98,7 @@ class EnrolmentController extends Controller
 
     public function store(Request $request)
     {
-        $data = $request->all();  dd($data);
+        $data = $request->all(); 
 
         if ($data['eligible_rd'] == 'is_eligible_mcq_osce') {
             $data['is_eligible_mcq_osce'] = 1;
@@ -121,11 +121,11 @@ class EnrolmentController extends Controller
 
             $courseinfo_id = $data['courseinfo_id'];  
 
-            $enrollment_check = $this->enrolment->getEnrollmentById($student_id,$courseinfo_id); 
+            // $enrollment_check = $this->enrolment->getEnrollmentById($student_id,$courseinfo_id);  dd($enrollment_check);
 
-            if(!is_null($enrollment_check)){  
-                return $enrollment_check->id;
-            }
+            // if(!is_null($enrollment_check)){  
+            //     return $enrollment_check->id;
+            // }
 
             $enrolmentData = array(
                 'student_id' => $student_id,
