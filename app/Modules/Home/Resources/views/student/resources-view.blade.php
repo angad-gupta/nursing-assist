69aa6ec404a98f900d4964a665e15aa136ccf827
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-11-12 10:56:22 +0545

    test

--- app/Modules/Home/Resources/views/student/resources-view.blade.php
+++ app/Modules/Home/Resources/views/student/resources-view.blade.php
@@ -7,6 +7,10 @@
   top: 0 !important; /* move doc up into empty bar space */
 }
 
+.toolbar {
+  display: none !important; /* hide PDF viewer toolbar */
+}
+
 </style>
 
 <embed src="{{asset($resources->file_full_path).'/'.$resources->source_name.'#toolbar=0&navpanes=0&scrollbar=0'}}" height="100%" width="100%">
