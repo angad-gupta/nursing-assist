56375b720fba39c72818dd9251449d2d960e2ec4
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-12-06 15:29:20 +0545

    test

--- app/Modules/Home/Resources/views/student/resources-view.blade.php
+++ app/Modules/Home/Resources/views/student/resources-view.blade.php
@@ -1,18 +1,33 @@
+@include('home::layouts.navbar-inner')
 
-<html>
-<head>
-<title></title>
+<iframe src="{{asset($resources->file_full_path).'/'.$resources->source_name.'#toolbar=0&navpanes=0'}}" height="100%" width="100%" id="ifr" /> 
+
+@include('home::layouts.footer')
+
+
+
+<style type="text/css">
+#outerContainer #mainContainer div.toolbar {
+  display: none !important; /* hide PDF viewer toolbar */
+}
+#outerContainer #mainContainer #viewerContainer {
+  top: 0 !important; /* move doc up into empty bar space */
+}
+
+.toolbar {
+	display: none !important;
+}
+
+
+</style>
+
+<!-- <embed src="{{asset($resources->file_full_path).'/'.$resources->source_name.'#toolbar=0&navpanes=0&scrollbar=0'}}" height="100%" width="100%" oncontextmenu="return false" > -->
+
+
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script type="text/jscript">
-  function disableContextMenu()
-  {
-    window.frames["fraDisabled"].document.oncontextmenu = function(){alert("No way!"); return false;};   
-    // Or use this
-    // document.getElementById("fraDisabled").contentWindow.document.oncontextmenu = function(){alert("No way!"); return false;};;    
-  }  
+<script type="text/javascript">
+	window.frames["ifr"].contentDocument.oncontextmenu = function(){
+	 return false; 
+	};
 </script>
-</head>
-<body bgcolor="#FFFFFF" onload="disableContextMenu();" oncontextmenu="return false">
-<iframe id="fraDisabled" height="100%" width="100%" src="{{asset($resources->file_full_path).'/'.$resources->source_name.'#toolbar=0&navpanes=0'}}" onload="disableContextMenu();" onMyLoad="disableContextMenu();"></iframe>
-</body>
-</html>
+
