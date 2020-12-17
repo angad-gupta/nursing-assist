a09245a76d8374e03b22653d793247f1b897292e
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-12-17 12:51:12 +0545

    test

--- app/Modules/Newsletter/Resources/views/newsletter/partial/email-content.blade.php
+++ app/Modules/Newsletter/Resources/views/newsletter/partial/email-content.blade.php
@@ -6,15 +6,32 @@
            
             <p style="font-family: sans-serif;padding-top: 20px;text-align: justify;font-size: 14px;padding-left: 20px;">{!! $message !!}</p>
 
+
             @if($attachment)
-                
+            
                 @php
                  $image = asset('uploads').'/newsletter/'.$attachment;
                 @endphp
-                <img src="{{$image}}" height="auto" width="auto" />
+                <a href="{{$image}}" target="_blank"><p style="font-family: sans-serif;padding-top: 20px;text-align: justify;font-size: 14px;padding-left: 20px;">Here is the Newsletter Attachment. Please view for Details :</p></a>
 
             @endif
 
+
+            <div class="detail" style="margin-bottom: 15px;display:flow-root;clear:both;background:#b30174;color:#fff;padding:20px">
+                <div class="lft" style="float:left">
+                    <p style="text-align: left;font-family: sans-serif;margin-left: 15px;font-size: 14px;font-weight: 600;margin-bottom: 5px;">Kind Regards,</p>
+                    <span style="text-align: left;font-family: sans-serif;margin-left: 15px;font-size: 14px;">The Team at NETA</span>
+                    <a href="mailto:info@nursingeta.com" style="display:block;font-family: sans-serif;padding-left: 15px;padding-top:5px;color:#fff">info@nursingeta.com</a>
+                    <a href="www.nursingeta.com" style="display:block;font-family: sans-serif;padding-left: 15px;padding-top:5px;color:#fff">www.nursingeta.com</a>
+                </div>
+
+                <div class="lft" style="float:right;padding-right:15px">
+                    <p style="text-align: left;font-family: sans-serif;margin-left: 15px;font-size: 14px;font-weight: 600;margin-bottom: 5px;">Contact Details</p>
+                    <span style="text-align: left;font-family: sans-serif;margin-left: 15px;font-size: 14px;">+61 432 430 170</span>
+                    
+                </div>
+            </div>
+            
             <p style="text-align: center;font-family: sans-serif;margin-left: 15px;font-size: 12px;padding: 15px 0;">© Nursingeta {{ date('Y') }} All rights reserved | <a href="www.nursingeta.com">nursingeta.com</a> |</p>
             
 
