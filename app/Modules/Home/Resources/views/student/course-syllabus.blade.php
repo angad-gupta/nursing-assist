1691e8e602d3a979b53bcfa1af0222c38891e82d
Shyam Sundar Awal <shyam.sundar.awal@bidhee.com>
2020-12-21 10:10:46 +0545

    Test

--- app/Modules/Home/Resources/views/student/course-syllabus.blade.php
+++ app/Modules/Home/Resources/views/student/course-syllabus.blade.php
@@ -29,9 +29,9 @@
             <div class="col-sm-12">
                 <div class="my-courses">
                     <h2 class="ttl-line">All syllabus</h2>
-                    @if($courseInfo->status == 0)
+                    <!-- @if($courseInfo->status == 0)
                         <p>Please pay remaining installment to access the syllabus! You might have received an email regarding the installments.</p>
-                    @else
+                    @else -->
                         <p>Find All Syllabus related to your Courses.</p>
                         <div class="row">
 
@@ -56,7 +56,7 @@
                                 @endforeach
                             @endif
                         </div>
-                    @endif
+                    <!-- @endif -->
                 </div>
             </div>
 
